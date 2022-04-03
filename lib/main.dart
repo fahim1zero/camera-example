a5c5bb22be91af28c22a67f2986f4d4733505259
Multajim Bin Kamal Fahim <fahimbinkamal2210@gmail.com>
2022-04-03 11:20:08 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: CustomNavigationBar()
+    );
+  }
+}
+
+class CustomNavigationBar extends StatelessWidget {
+  const CustomNavigationBar({Key? key}) : super(key: key);
+  static late double screenWidth;
+  static late double screenHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: screenWidth * 0.25,
+      height: screenHeight * 0.05,
+      decoration: BoxDecoration(
+          color: Colors.white30,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20),
+            topLeft: Radius.circular(20),
+          )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.message),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
+  }
+}
